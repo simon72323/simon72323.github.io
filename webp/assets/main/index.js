f4acb2e4955dc2b78cd08ea4ca8e91ca51d01820
simonBear <64388324+simon72323@users.noreply.github.com>
2025-06-13 10:51:27 +0800

    Add files via upload

---
+++ webp/assets/main/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/main",["./spineRunInEditor.ts"],(function(){return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/spineRunInEditor.ts",["cc"],(function(){var n;return{setters:[function(e){n=e.cclegacy}],execute:function(){n._RF.push({},"a7e412pQSlER5GbvlHjsyiJ","spineRunInEditor",void 0),n._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/main', 'chunks:///_virtual/main'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
